250e8fd46d7d6de6896c23b42b4a78a90ddeca18
Lukas Schmidt <primeapi@primeapi.de>
2022-08-24 01:07:53 +0200

    Added NoHunger Perk #2w3anuv

---
+++ src/main/java/de/primeapi/primeplugins/skypvp/listeners/HungerLevelChangeListener.java
@@ -0,0 +1,27 @@
+package de.primeapi.primeplugins.skypvp.listeners;
+
+import de.primeapi.primeplugins.skypvp.sql.stats.Perk;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+
+/**
+ * @author Lukas S. PrimeAPI
+ * created on 24.08.2022
+ * crated for SkyPvP
+ */
+public class HungerLevelChangeListener implements Listener {
+
+	@EventHandler
+	public void onHungerLevelChange(FoodLevelChangeEvent event){
+		if(event.getEntity() instanceof Player){
+			Player player = (Player) event.getEntity();
+			if(Perk.NO_HUNGER.isActive(player)){
+				event.setCancelled(true);
+			}
+		}
+
+	}
+
+}
